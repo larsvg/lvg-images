7817c185fc8cfbd6a09507fea3f423e7f37088cf
larsvangalen <lars@vangalen.org>
2020-08-31 11:19:28 +0200

    Save source image

--- src/Http/ImageProcessor/ImageProcessor.php
+++ src/Http/ImageProcessor/ImageProcessor.php
@@ -3,14 +3,53 @@
 namespace larsvg\lvgimages\Http\ImageProcessor;
 
 
+use Illuminate\Http\File;
 use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+use phpDocumentor\Reflection\Types\Void_;
+use Spatie\Glide\GlideImage;
 
 abstract class ImageProcessor
 {
 
-    public function __construct(UploadedFile $image, int $width, int $height)
-    {
+    protected UploadedFile $image;
+    protected ?int         $width;
+    protected ?int         $height;
+    protected string       $targetFolder;
+    protected string       $originalImage;
 
+    /**
+     * ImageProcessor constructor.
+     *
+     * @param UploadedFile $image
+     * @param int|null     $width
+     * @param int|null     $height
+     * @param string       $targetFolder
+     */
+    public function __construct(UploadedFile $image, int $width = null, int $height = null, string $targetFolder = '')
+    {
+        $this->height       = $height;
+        $this->width        = $width;
+        $this->image        = $image;
+        $this->targetFolder = public_path('images') . $targetFolder;
+
+        $this->saveOriginalImage();
+
+        $this->scaleImage();
     }
+
+    /**
+     * @return string
+     */
+    private function saveOriginalImage(): string
+    {
+        $name = 'original.' . $this->image->clientExtension();
+        $this->image->move($this->targetFolder, $name);
+        $this->originalImage = $this->targetFolder . '/' . $name;
+
+        return $this->originalImage;
+    }
+
+    abstract protected function scaleImage();
 
 }
--- src/Http/ImageProcessor/JpgProcessor.php
+++ src/Http/ImageProcessor/JpgProcessor.php
@@ -2,9 +2,16 @@
 
 namespace larsvg\lvgimages\Http\ImageProcessor;
 
+use Spatie\Glide\GlideImage;
 
 class JpgProcessor extends ImageProcessor
 {
 
+    protected function scaleImage()
+    {
+        dd(GlideImage::create($this->originalImage)
+            ->modify(['w' => $this->width])
+            ->save($this->targetFolder.'/scaled.jpg'));
+    }
 
 }
